18d4ae172f0070f50e99b1bef10b161d0fee6141
Oguz Bilgic <fisyonet@gmail.com>
2014-03-27 04:33:42 -0400

    Add tradeEngine()

--- client.go
+++ client.go
@@ -2,15 +2,30 @@ package marketstub
 
 import (
 	"errors"
+	"github.com/oguzbilgic/fpd"
 	"github.com/oguzbilgic/market"
+	"math/rand"
 	"time"
 )
 
 type Client struct {
+	tradeChans []chan *market.Trade
 }
 
 func New() *Client {
-	return &Client{}
+	client := &Client{}
+
+	trades := tradeEngine()
+	go func() {
+		for {
+			trade := <-trades
+			for _, tradeChan := range client.tradeChans {
+				tradeChan <- trade
+			}
+		}
+	}()
+
+	return client
 }
 
 func (c *Client) Ticker() (*market.Tick, error) {
@@ -31,11 +46,32 @@ func (c *Client) NewDepthChan() chan *market.Depth {
 
 func (c *Client) NewTradeChan() chan *market.Trade {
 	tradeChan := make(chan *market.Trade)
+	c.tradeChans = append(c.tradeChans, tradeChan)
+	return tradeChan
+}
+
+func tradeEngine() chan *market.Trade {
+	rand.Seed(time.Now().UnixNano())
+	tradePrice := fpd.NewFromFloat(rand.Float64()*100, -3)
+	tradeChan := make(chan *market.Trade)
 
 	go func() {
 		for {
-			time.Sleep(2 * time.Second)
-			tradeChan <- stubTrade()
+			duration := time.Duration(rand.Float32() * 2000)
+			time.Sleep(duration * time.Millisecond)
+
+			tradePrice := tradePrice.Add(fpd.NewFromFloat(rand.Float64(), -2))
+			tradeVolume := fpd.NewFromFloat(rand.Float64()*10, -2)
+
+			trade := &market.Trade{
+				Symbol:   "market:stubUSD",
+				Currency: market.USD,
+				Volume:   tradeVolume,
+				Price:    tradePrice,
+				Time:     time.Now(),
+			}
+
+			tradeChan <- trade
 		}
 	}()
 
